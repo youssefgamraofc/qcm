64714610826cc5a0a586e495a8f981d6077c956c
youssefgamraofc <spynoza.ofc@gmail.com>
2020-05-22 00:58:50 +0100

    Model, Controller and Migration for questions table are set

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    QCM Maroc
                 </div>
 
                 <div class="links">
